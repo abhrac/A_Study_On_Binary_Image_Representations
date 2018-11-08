aa35608f38b8eed1b3443a4536f233e77324d804
iabhra <rick.abhra@gmail.com>
2018-11-08 14:43:15 +0530

    small changes

--- source/run_forest_to_quadtree.py
+++ source/run_forest_to_quadtree.py
@@ -1,3 +1,7 @@
+# Program to convert a run-forest to its corresponding quadtree representation
+# Usage: python run_forest_to_quadtree.py path_to_run_forest_file 
+# Example: python run_forest_to_quadtree.py ../run_forest_representations/1024x1024/scenery_1_1024x1024_run_forest.txt
+
 from split_run_forests import *
 from reconstruct_from_quadtree import *
 from utils.parse_utils import *
@@ -33,16 +37,21 @@ def test_homogeneity(rf):
     return(True, color(col_0))
 
 def recursively_decompose_rf(rf, code, leaves):
+    # Function to recursively decompose a run-forest into
+    # its corresponding quadtree
     homogeneous, color = test_homogeneity(rf)
+    # If a block is either completely white or completely black,
+    # add it to the list of quadtree leaves
     if (homogeneous):
         leaves.append((code, (color * 1)))
         return leaves
-
+    # Horizontally split the run-forest into upper and lower halves
     upper_half, lower_half = horz_split_rf(rf)
-
+    # Vertically split the upper and lower halves to finally obtain
+    # the 4 image quadrants
     quad_0, quad_1 = vert_split_rf(upper_half)
     quad_2, quad_3 = vert_split_rf(lower_half)
-
+    # Recursively decompose each of the 4 quadrants
     leaves = recursively_decompose_rf(quad_0, (code + '0'), leaves)
     leaves = recursively_decompose_rf(quad_1, (code + '1'), leaves)
     leaves = recursively_decompose_rf(quad_2, (code + '2'), leaves)
@@ -53,31 +62,32 @@ def run_forest_to_quadtree(rf):
     leaves = recursively_decompose_rf(rf, '0', [])
     return (rf[0], leaves)
 
-def main():
+def get_rf_path_from_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('rf_path', type=str)
     args = parser.parse_args()
     rf_path = args.rf_path
+    return rf_path
 
-    rf = parse_run_forest_file(rf_path)
-    
-    quadtree = run_forest_to_quadtree(rf)
-    
+def save_quadtree_from_rf(rf_path, quadtree):
     target_path = '../quadtree_codes/from_run_forests/'
-    
     qt_code_path = save_quadtree_codes(rf_path, quadtree, target_path)
+    return qt_code_path
 
-    quadtree = parse_qt_code_file(qt_code_path)
-
+def convert_to_binary_and_save(quadtree, qt_code_path):
     reconstructed_im = quadtree_to_binary(quadtree)
-
     Image.fromarray(reconstructed_im * 255).show()
-
     target_path = '../reconstructed_images/run_forest_to_quadtree/'
-
     im_prefix = 'rf_to_qt_'
-
     save_reconstructed_im(reconstructed_im, qt_code_path, target_path, im_prefix)
+
+def main():
+    rf_path = get_rf_path_from_args()
+    rf = parse_run_forest_file(rf_path)
+    quadtree = run_forest_to_quadtree(rf)
+    qt_code_path = save_quadtree_from_rf(rf_path, quadtree)
+    quadtree = parse_qt_code_file(qt_code_path)
+    convert_to_binary_and_save(quadtree, qt_code_path)
 
 if __name__ == '__main__':
     main()
--- source/split_run_forests.py
+++ source/split_run_forests.py
@@ -1,13 +1,20 @@
+# Program to vertically and horizontally split a given run-forest
+# Usage: python split_run_forests.py path_to_run_forest_file 
+# Example: python split_run_forests.py ../run_forest_representations/1024x1024/scenery_1_1024x1024_run_forest.txt
+
 from PIL import Image
 from reconstruct_from_run_forest import *
 import argparse
 
 def decrement_column_indices(col, l):
+    # Function to decrement the run indices of col by l
     for (i, run) in enumerate(col):
         col[i] = (run[0] - l), (run[1] - l)
     return col
 
 def remove_null_runs(upper, lower):
+    # Function to remove runs that start and end in
+    # the same pixel
     if (upper[-1][0] == upper[-1][1]):
         upper = upper[:-1]
     if (lower[0][0] == lower[0][1]):
@@ -15,6 +22,7 @@ def remove_null_runs(upper, lower):
     return (upper, lower)
 
 def split_column(col, l):
+    # Function to split a run-forest column into 2 halves
     mid = int(l / 2)
     for (i, run) in enumerate(col):
         if (run[0] <= mid <= run[1]):
@@ -32,9 +40,9 @@ def split_column(col, l):
     return(upper, lower)
 
 def horz_split_rf(rf):
+    # Function to horizontally split a run-forest
     n_rows, n_cols = rf[0]
     rf_cols = rf[1]
-
     split_im_dims = int(n_rows / 2), n_cols
     u_rf_cols, l_rf_cols = [], []
     for col in rf_cols:
@@ -45,35 +53,53 @@ def horz_split_rf(rf):
     return (u_rf, l_rf)
 
 def vert_split_rf(rf):
+    # Function to vertically split a run-forest
     n_rows, n_cols = rf[0]
     rf_cols = rf[1]
-
     split_im_dims = n_rows, int(n_cols/2)
-
     l_rf_cols, r_rf_cols = rf_cols[:int(n_cols/2)], rf_cols[int(n_cols/2):]
     l_rf, r_rf = (split_im_dims, l_rf_cols), (split_im_dims, r_rf_cols)
     return(l_rf, r_rf)
 
-def main():
+def get_rf_path_from_arguments():
+    # Function to parse command-line arguments
     parser = argparse.ArgumentParser()
     parser.add_argument('rf_path', type=str)
     args = parser.parse_args()
-    rf_path = args.rf_path
+    return args.rf_path
 
+def binarize_all(run_forests):
+    # Function to convert a list of run-forests to their
+    # corresponding binary images
+    bin_ims = []
+    for rf in run_forests:
+        bin_ims.append(run_forest_to_binary(rf))
+    return (bin_ims)
+
+def display_binary_images(images):
+    # Function to display a list of binary images
+    for im in images:
+        Image.fromarray(im * 255).show()
+
+def main():
+    # Get path to run-forest file from command-line arguments
+    rf_path = get_rf_path_from_arguments()
+
+    # Get run-forest file from parsed command-line arguments
     rf = parse_run_forest_file(rf_path)
-
+    
+    # Split the given run-forest vertically
     left_rf, right_rf = vert_split_rf(rf)
-
-    bin_left = run_forest_to_binary(left_rf)
-    bin_right = run_forest_to_binary(right_rf)
-
+    
+    # Split the given run-forest horizontally
     upper_rf, lower_rf = horz_split_rf(rf)
+    
+    # Get corresponding binary images from the horizontally
+    # split run-forests
+    bin_left, bin_right, bin_upper, bin_lower = binarize_all([left_rf, right_rf, upper_rf, lower_rf])
 
-    bin_upper = run_forest_to_binary(upper_rf)
-    bin_lower = run_forest_to_binary(lower_rf)
-
-    Image.fromarray(bin_upper * 255).show()
-    Image.fromarray(bin_lower * 255).show()
+    # Display images obtained from vertical and horizontal splitting
+    display_binary_images([bin_left, bin_right, bin_upper, bin_lower])
 
 if __name__ == '__main__':
     main()
